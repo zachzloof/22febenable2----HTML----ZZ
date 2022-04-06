fe2c3c40d273a2b8e0c3b51a7784402012b932f3
Zach Zloof <zachzloof@hotmail.com>
2022-04-06 20:49:22 +0100

    Late nate additions

--- calculator_startpoint/startpoint/calculator.js
+++ calculator_startpoint/startpoint/calculator.js
@@ -2,6 +2,7 @@
 import * as DOM from './dom.js';
 
 let total = 0;
+let equation = "";
 let add = () => {
     let input1 = DOM.inputOne.value;
     let input2 = DOM.inputTwo.value;
@@ -11,6 +12,7 @@ let add = () => {
     total = total + parseInt(input1) + parseInt(input2);
     child.innerHTML = total;
     //child.innerHTML = parseInt(input1) + " + " + parseInt(input2) + " = " + `${parseInt(input1) + parseInt(input2)}`
+    equation = equation + ` + ` + parseInt(input1) + ` + ` + parseInt(input2);
     document.getElementById(`list_output2`).appendChild(child);
     
 };
@@ -23,6 +25,7 @@ let sub = () => {
     total = total - parseInt(input1) - parseInt(input2);
     child.innerHTML = total;
     //child.innerHTML = parseInt(input1) + " - " + parseInt(input2) + " = " + `${parseInt(input1) - parseInt(input2)}`
+    equation = equation + ` - ` + parseInt(input1) + ` - ` + parseInt(input2);
     document.getElementById(`list_output2`).appendChild(child);
 };
 let div = () => {
@@ -34,6 +37,7 @@ let div = () => {
     total = total / parseInt(input1) / parseInt(input2);
     child.innerHTML = total;
     //child.innerHTML = parseInt(input1) + " / " + parseInt(input2) + " = " + `${parseInt(input1) / parseInt(input2)}`
+    equation = equation + ` / ` + parseInt(input1) + ` / ` + parseInt(input2);
     document.getElementById(`list_output2`).appendChild(child);
 };
 let mul = () => {
@@ -45,15 +49,15 @@ let mul = () => {
     total = total * parseInt(input1) * parseInt(input2);
     child.innerHTML = total;
     //child.innerHTML = parseInt(input1) + " x " + parseInt(input2) + " = " + `${parseInt(input1) * parseInt(input2)}`
+    equation = equation + ` * ` + parseInt(input1) + ` * ` + parseInt(input2);
     document.getElementById(`list_output2`).appendChild(child);
 };
 let reset = () => {
-    let input1 = DOM.inputOne.value;
-    let input2 = DOM.inputTwo.value;
     let list = document.getElementById(`list_output`);
     list.innerHTML = ``;
-    let parent = document.querySelector(`ul`);
-    parent.removeChild(list);
+    document.getElementById(`main`).appendChild(list);
+    total = 0;
+    equation = "";
 }
 let result = () => {
     //let total2 = DOM.textOutput.value;
@@ -61,10 +65,20 @@ let result = () => {
     let child2 = document.getElementById(`listoutput2`);
     child2.innerHTML = 0;
     child.classList.add(`list-group-item`);
-    child.innerHTML = total;
+    child.innerHTML = equation + ` = ` + total;
     document.getElementById(`list_output`).appendChild(child);
     document.getElementById(`list_output2`).appendChild(child2);
     total = 0;
+    equation = "";
+}
+let danger = () => {
+    let body = document.querySelector('body');
+    let message = document.createElement(`h1`);
+    message.innerHTML = `I said dont touch the bumbaclart button >:(`;
+    body.style.color = 'red';
+    body.style.backgroundColor = 'black';
+    document.querySelector(`html`).appendChild(body);
+    document.getElementById(`disDiv`).appendChild(message);
 }
 
 DOM.buttonAdd.onclick = () => add();
@@ -73,3 +87,4 @@ DOM.buttonMul.onclick = () => mul();
 DOM.buttonSub.onclick = () => sub();
 DOM.buttonReset.onclick = () => reset();
 DOM.buttonResult.onclick = () => result();
+DOM.buttonDisobedience.onclick = () => danger();
